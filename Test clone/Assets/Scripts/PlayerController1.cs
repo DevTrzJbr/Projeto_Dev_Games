f542cbc9bed7028603ed89e6918b2c5dcbdb6056
João  Víctor Antunes <joao.extremo722@gmail.com>
2023-04-15 18:19:37 -0300

    Teste com animações

---
+++ Test clone/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody rb;
+
+    private CharacterController character; // Referência ao componente CharacterController do personagem
+    private Animator animator; // Referência ao componente Animator do personagem
+    private Vector3 inputs; // Vetor que armazena as entradas de movimento do jogador
+
+    GameObject groundObject;
+
+
+    // variável para verificar se o jogador está no chão
+    private bool isGrounded = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        character = GetComponent<CharacterController>(); // Obtém a referência ao componente CharacterController do personagem
+        animator = GetComponent<Animator>(); // Obtém a referência ao componente Animator do personagem
+
+        groundObject = GameObject.FindGameObjectWithTag("ground");
+
+        TrapSpikes trapSpikes = FindObjectOfType<TrapSpikes>();
+        if (trapSpikes != null)
+        {
+            trapSpikes.OnPlayerEnterTrap += OnEnterTrap;
+        }
+    }
+
+    private void OnEnterTrap(PlayerController player)
+    {
+        Debug.Log("O jogador entrou na armadilha!");
+        // Faça algo quando o jogador entrar na armadilha
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(horizontal, 0f, 0f) * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) // verifica se o jogador está no chão antes de permitir o pulo
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            animator.SetBool("jump", true);
+        }
+
+        if (Physics.Raycast(transform.position, transform.right, 1f))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+    
+    // verifica se o jogador está no chão
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    // verifica se o jogador saiu do chão
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            animator.SetBool("jump", false);
+        }
+    }
+}
