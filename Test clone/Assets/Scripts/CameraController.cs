7ca024445552b812f113fe0c6b2b4d7d07b9b5ec
João  Víctor Antunes <joao.extremo722@gmail.com>
2023-04-16 18:01:57 -0300

    Add Angle on Camera

--- Test clone/Assets/Scripts/CameraController.cs
+++ Test clone/Assets/Scripts/CameraController.cs
@@ -8,9 +8,12 @@ public class CameraController : MonoBehaviour
 
     public float smoothSpeed = 0.125f; // velocidade de movimento da câmera
     public Vector3 offset; // distância da câmera em relação ao jogador
-    public float minHeight = 1.0f; // altura mínima da câmera em relação ao jogador
+    public float minHeight = 0.5f; // altura mínima da câmera em relação ao jogador
+    public float viewAngle = 30f; // Angulo de visão para o player
+    [Range(0.5f, 2.5f)]
+    public float cameraHeight = 1.5f; // altura da câmera em relação ao jogador
 
-    void FixedUpdate()
+    void Update()
     {
         Vector3 desiredPosition = player.position + offset; // posição que a câmera deve estar
 
@@ -19,7 +22,11 @@ public class CameraController : MonoBehaviour
             desiredPosition.y = player.position.y + minHeight; // mantém a câmera a uma altura mínima em relação ao jogador
         }
 
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // suaviza o movimento da câmera
+        Vector3 cameraOffset = Quaternion.Euler(viewAngle, 0f, 0f) * offset; // adiciona uma rotação à câmera
+        Vector3 desiredCameraPosition = player.TransformPoint(cameraOffset); // adiciona a rotação à posição desejada da câmera
+        desiredCameraPosition += Vector3.up * cameraHeight; // adiciona a altura da câmera à posição desejada da câmera
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredCameraPosition, smoothSpeed); // suaviza o movimento da câmera
         transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z); // move a câmera
     }
 }
